1f955f5e4953825fa4ac24bfc963c16ddd4e6e6d
Ecliße <rafaumar22@gmail.com>
2023-06-28 20:47:09 +0700

    Removed the 'test' page.

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,6 +2,6 @@
     <strong>Copyright &copy; 2023 <a href="">Demo</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 1.3.0
+        <b>Version</b> 2.0.0
     </div>
 </footer>
